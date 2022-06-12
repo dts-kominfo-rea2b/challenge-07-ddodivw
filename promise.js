c264ff65d11163868d6440a60c316479d57f7aba
ddorest <ddodivw@gmail.com>
2022-06-12 23:32:28 +0700

    update promise.js

--- promise.js
+++ promise.js
@@ -1,7 +1,19 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async(mood) => {
+  try {
+    let theaterIXX = await promiseTheaterIXX()
+    let theaterVGC = await promiseTheaterVGC()
+
+    let moodPenonton = [...theaterIXX, ...theaterVGC]
+    let result = moodPenonton.filter((hasilMood=> hasilMood.hasil === mood)).length
+    return result;
+  } 
+  catch (err) {
+    console.log("Error :"+err)
+  }
+};
 
 module.exports = {
   promiseOutput,
